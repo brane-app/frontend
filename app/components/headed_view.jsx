a6ab547416283989b249472c20c87c971544f715
Noah Harris <dakoolstwunn@gmail.com>
2020-09-12 05:23:44 -0400

    Add profile view wrapped in a navigator
    Add a profile view wrapped in a navigator that opens the navigation
    modal

---
+++ app/components/headed_view.jsx
@@ -0,0 +1,163 @@
+import React from "react"
+import {
+    Modal,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native"
+
+import style_generic from "../style/generic"
+import style_headed_view from "../style/component/headed_view"
+import style_nav_modal from "../style/component/nav_modal"
+import style_cross from "../style/drawing/cross"
+
+const style = {
+    generic: style_generic,
+    headed_view: style_headed_view,
+    nav_modal: style_nav_modal,
+    cross: style_cross
+}
+
+class Close extends React.Component {
+    constructor(opts = {}) {
+        super(opts)
+        this._onPress = opts.onPress || (() => {})
+    }
+
+    get style() {
+        return [
+            style.cross.cross,
+            {
+                transform: [
+                    {rotate: "45deg"},
+                ],
+            }
+        ]
+    }
+
+    render() {
+        return (
+            <View style = {this.style}>
+                <View style = {style.cross.horizontal_line}/>
+                <View style = {style.cross.vertical_line}/>
+            </View>
+        )
+    }
+}
+
+class NavButton extends React.Component {
+    constructor (opts = {}) {
+        super(opts)
+        this._text = opts.text || ""
+        this._onPress = opts.onPress || (() => {})
+    }
+
+    render () {
+        return (
+            <TouchableOpacity
+                onPress = {this._onPress}
+                style = {style.nav_modal.button}>
+                <Text style = {[style.generic.text_ui, style.nav_modal.text]}>
+                    {this._text}
+                </Text>
+            </TouchableOpacity>
+        )
+    }
+}
+
+class HeadedView extends React.Component {
+    constructor(opts) {
+        super(opts)
+        this.state = {modal_visible: false}
+    }
+
+    get modal_visible () {
+        return this.state.modal_visible
+    }
+
+    set modal_visible (value) {
+        this.setState({
+            modal_visible: value
+        })
+    }
+
+    get header() {
+        return (
+            <View style = {style.headed_view.header}>
+                <TouchableOpacity
+                    style = {style.headed_view.header_contain}
+                    onPress = {() => {this.modal_visible = true}}>
+                    <Text style = {[style.generic.text_ui, style.headed_view.text]}>
+                        {this.name}
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    get content () {
+        return <View/>
+    }
+
+    get nav_close () {
+        return (
+            <TouchableOpacity
+                onPress = {() => {this.modal_visible = false}}
+                style = {style.nav_modal.close}>
+                <Close/>
+            </TouchableOpacity>
+        )
+    }
+
+    get nav_buttons () {
+        return (
+            <>
+            <NavButton
+                text = {"New"}
+                onPress = {this.navigate_to("feed_new")}/>
+            <NavButton
+                text = {"Profile"}
+                onPress = {this.navigate_to("profile")}/>
+            </>
+        )
+    }
+
+    get nav_modal () {
+        return (
+            <Modal
+                transparent = {true}
+                visible = {this.modal_visible}>
+                <TouchableOpacity
+                    onPress = {() => {this.modal_visible = false}}
+                    activeOpacity = {1}
+                    style = {style.nav_modal.dim}>
+                    <View style = {style.nav_modal.contain}>
+                        {this.nav_close}
+                        {this.nav_buttons}
+                    </View>
+                </TouchableOpacity>
+            </Modal>
+        )
+    }
+
+    navigate_to(where) {
+        return () => {
+            this.modal_visible = false
+            this.props.navigation.replace(where)
+        }
+    }
+
+    render () {
+        return (
+            <View style = {style.headed_view.contain}>
+                {this.nav_modal}
+                {this.header}
+                <View style = {style.headed_view.content_contain}>
+                    {this.content}
+                </View>
+            </View>
+        )
+    }
+}
+
+export default HeadedView
--- app/views/profile.jsx
+++ app/views/profile.jsx
@@ -1,42 +1,199 @@
 import React from "react"
 import {
-    View
+    Image,
+    View,
+    Text,
 } from "react-native"
 
 import imonke from "imonke"
 
-import style_generic from "../style/generic"
+import HeadedView from "../components/headed_view"
 import global_state from "../state"
+import style_generic from "../style/generic"
+import style_profile from "../style/views/profile"
 
 const style = {
     generic: style_generic,
+    profile: style_profile,
 }
+
 
 class ProfileCard extends React.Component {
-    constructor (opts = {}) {
+    constructor(opts = {}) {
         super()
+
         this.client = opts.client
+        this.state = {
+            client_data: null
+        }
     }
 
-    render () {
-        return <View/>
+    async componentDidMount() {
+        this.setState({
+            client_data: await this.client.data
+        })
+    }
+
+    get client_data() {
+        return this.state.client_data
+    }
+
+    get loaded() {
+        return [
+            this.client_data,
+        ].every(it => it)
+    }
+
+    get loading_view() {
+        return (
+            <>
+            </>
+        )
+    }
+
+    get profile_image() {
+        return (
+            // We don't have pfp's yet :(
+            // But we will soon
+            <Image style = {style.profile.image}/>
+        )
+    }
+
+    get since() {
+        return (
+            <View style = {style.profile.since_contain}>
+                <Text style = {[style.generic.text_ui, style.profile.since]}>
+                    {"Joined MM/YY"}
+                </Text>
+            </View>
+        )
+    }
+
+    get info() {
+        return (
+            <>
+            <View style = {style.profile.info}>
+                <View style = {style.profile.nick_contain}>
+                    <Text style = {[style.generic.text_ui, style.profile.nick]}>
+                        {this.client_data.nick}
+                    </Text>
+                </View>
+                <View style = {style.profile.stats_contain}>
+                    {this.stat("subscribers", this.client_data.subscriber_count)}
+                    {this.stat("subscriptions", this.client_data.subscription_count)}
+                    {this.stat("posts", this.client_data.post_count)}
+                </View>
+            </View>
+            </>
+        )
+    }
+
+    get loaded_view() {
+        return (
+            <>
+            <View style = {style.profile.info_contain}>
+                {this.profile_image}
+                {this.info}
+            </View>
+            {this.since}
+            </>
+        )
+    }
+
+    stat(label, data) {
+        return (
+            <View style = {style.profile.stat}>
+                <Text style = {[style.generic.text_ui, style.profile.stat_data]}>
+                    {data}
+                </Text>
+                <Text style = {[style.generic.text_ui, style.profile.stat_label]}>
+                    {label}
+                </Text>
+            </View>
+
+        )
+    }
+
+    render() {
+        return (
+            <View style = {style.profile.card_contain}>
+                {this.loaded ? this.loaded_view : this.loading_view}
+            </View>
+        )
     }
 }
 
-class ProfileView extends React.Component {
-    constructor (opts = {}) {
-        super(opts)
-        this.client = opts.client || global_state.client || (new imonke.Client())
+class ProfileContent extends React.Component {
+    constructor(opts = {}) {
+        super()
+
+        this.client = opts.client
+        this.state = {
+            content_generator: null
+        }
     }
 
-    get profile_card () {
+    get content() {
+        return this.state.content_generator
+    }
+
+    get loading_view() {
+        return (
+            <View style = {style.profile.content_loading}>
+                <Text style = {style.profile.content_loading_text}>
+                    {"I know, I know. It's on the way"}
+                </Text>
+            </View>
+        )
+    }
+
+    get loaded_view() {
+        return
+    }
+
+    render() {
+        return (
+            <View style = {style.profile.content_contain}>
+                {this.loaded ? this.loaded_view : this.loading_view}
+            </View>
+        )
+    }
+}
+
+class ProfileView extends HeadedView {
+    constructor(opts = {}) {
+        super(opts)
+
+        this.state = {
+            ...this.state,
+            name: "profile",
+        }
+        this.client = opts.client || global_state.client
+    }
+
+    async componentDidMount() {
+        this.setState({
+            name: await this.client.nick
+        })
+    }
+
+    get name() {
+        return this.state.name
+    }
+
+    get profile_card() {
         return <ProfileCard client = {this.client}/>
     }
 
-    render () {
+    get profile_content() {
+        return <ProfileContent client = {this.client}/>
+    }
+
+    get content () {
         return (
-            <View style = {style.generic.view}>
+            <View style = {[style.generic.view, style.profile.profile_view]}>
                 {this.profile_card}
+                {this.profile_content}
             </View>
         )
     }
