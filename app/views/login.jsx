9e6222c732aaf71963eeddf1e1f8b81911d43f5a
Noah Harris <dakoolstwunn@gmail.com>
2020-12-14 02:05:29 -0500

    Restructure pane switching
    Restructure pane switching such that instead of being either type login
    or not, the state field submit_type can be any of login, register,
    continue. This allows us to add more than the login / register panes to
    the login view

--- app/views/login.jsx
+++ app/views/login.jsx
@@ -24,13 +24,20 @@ const email_regex = /^[^@]+@[^@]+$/
 class LoginView extends HeadedView {
     constructor(opts = {}) {
         super(opts)
-        this.state = { ...this.state, login: false, submit_disabled: false }
-
+        this.state = { ...this.state, submit_type: "register", submit_disabled: false }
         this.client = new Client()
         this.input_nick = null
         this.input_email = null
         this.input_password = null
         this.submit_button_ref = null
+    }
+
+    get submit_type() {
+        return this.state.submit_type
+    }
+
+    get submit_disabled() {
+        return this.state.submit_disabled
     }
 
     async display(message) {
@@ -60,10 +67,10 @@ class LoginView extends HeadedView {
     async submit_login() {
         const fields = [ "email", "password" ]
         if (!await this.valid(fields)) {
-            return
+            return false
         }
 
-        if ( await this.client.login(await this.entries(fields)) ) {
+        if (await this.client.login(await this.entries(fields))) {
             return true
         } else {
             this.display("bad credentials")
@@ -75,39 +82,50 @@ class LoginView extends HeadedView {
     async submit_register() {
         const fields = [ "nick", "email", "password" ]
         if (!await this.valid(fields)) {
-            return
+            return false
         }
 
-        if( await this.client.create(await this.entries(fields)) ) {
-            return true
-        }
+        return await this.client.create(await this.entries(fields))
+    }
+
+    async submit_continue() {
+        return false
     }
 
     async submit() {
+        let result
+        switch (this.submit_type) {
+            case "login":
+                result = await this.submit_login()
+                break
+            case "register":
+                result = await this.submit_register()
+                break
+            case "continue":
+                result = await this.submit_continue()
+                break
+        }
+        if (result) {
+            global_state.client = this.client
+            this.props.navigation.replace("profile")
+        }
+    }
+
+    async submit_wrapped() {
         this.setState(
             { submit_disabled: true },
             async () => {
                 try {
-                    if (this.login ? await this.submit_login() : await this.submit_register()) {
-                        global_state.client = this.client
-                        this.props.navigation.replace("profile")
+                    if (!await this.submit()) {
+                        this.display(`failed to ${this.submit_type}`)
                     }
-                } catch (err) {
-                    this.display(`failed to ${this.login ? "login" : "register"}`)
+                } catch {
+                    this.display(`error trying to ${this.submit_type}`)
                 } finally {
                     this.setState({ submit_disabled: false })
                 }
-
             }
         )
-    }
-
-    get login() {
-        return this.state.login
-    }
-
-    get submit_disabled() {
-        return this.state.submit_disabled
     }
 
     get header() {
@@ -118,15 +136,16 @@ class LoginView extends HeadedView {
         return (
             <View style = { style.login.top_contain }>
                 <TouchableOpacity
-                    onPress = { () => this.setState({ login: false }) }
-                    style = {[style.login.top_switcher, this.login ? {} : style.login.top_switcher_underline]}>
+                    onPress = { () => this.setState({ submit_type: "register" }) }
+                    style = {[style.login.top_switcher, this.submit_type == "register" ? style.login.top_switcher_underline : {}]}>
                     <Text style = {[ style.generic.text_ui, style.login.button_text ]}>
                     {"register"}
                     </Text>
                 </TouchableOpacity>
                 <TouchableOpacity
-                    onPress = { () => this.setState({ login: true }) }
-                    style = {[style.login.top_switcher, this.login ? style.login.top_switcher_underline : {}]}>
+                    onPress = { () => this.setState({ submit_type: "login" }) }
+                    // TODO This is ugly
+                    style = {[style.login.top_switcher, this.submit_type == "login" ? style.login.top_switcher_underline : {} ]}>
                     <Text style = {[ style.generic.text_ui, style.login.button_text ]}>
                         {"login"}
                     </Text>
@@ -135,75 +154,105 @@ class LoginView extends HeadedView {
         )
     }
 
-    get inputs() {
+    get field_nick() {
         return (
-            <View style = { style.login.input_contain }>
-                {
-                    this.login ? null : <ValidInput
-                        ref = { (ref) => { this.input_nick = ref } }
-                        name = { "nick" }
-                        key = { "nick" }
-                        autoCompleteType = { "off" }
-                        returnKeyType = { "next" }
-                        onSubmitEditing = { () => this.input_email.focus() }
-                        style = {[  style.generic.text_field_ui, style.login.input ]}
-                        validator = { async (it) => {
-                            if (!nick_regex.test(it)) {
-                                return [ false, `${it} is invalid` ]
-                            }
-
-                            if (16 < it.length) {
-                                return [ false, `${it} is too long (${it.length} > 64)` ]
-                            }
-
-                            if (it.length < 4) {
-                                return [ false, `${it} is too short (${it.length} < 4)` ]
-                            }
-
-                            if ( !this.login && await this.client.nick_exists(it) ) {
-                                return [ false, `${it} is occupied` ]
-                            }
-
-                            return [ true, null ]
-                        } }/>
-                }
-                <ValidInput
-                    ref = { (ref) => { this.input_email = ref } }
-                    name = { "email" }
-                    key = { "email" }
-                    autoCompleteType = { "email" }
-                    returnKeyType = { "next" }
-                    onSubmitEditing = { () => this.input_password.focus() }
-                    style = {[  style.generic.text_field_ui, style.login.input ]}
-                    validator = { async (it) => {
-                        if (!email_regex.test(it)) {
-                            return [ false, `${it} isn't valid` ]
+            <ValidInput
+                ref = { (ref) => { this.input_nick = ref } }
+                name = { "nick" }
+                key = { "nick" }
+                autoCompleteType = { "off" }
+                returnKeyType = { "next" }
+                onSubmitEditing = { () => this.input_email.focus() }
+                style = {[  style.generic.text_field_ui, style.login.input ]}
+                validator = {
+                    async (it) => {
+                        if (!nick_regex.test(it)) {
+                            return [ false, `${it} is invalid` ]
                         }
 
-                        if ( !this.login && await this.client.email_exists(it) ) {
+                        if (16 < it.length) {
+                            return [ false, `${it} is too long (${it.length} > 64)` ]
+                        }
+
+                        if (it.length < 4) {
+                            return [ false, `${it} is too short (${it.length} < 4)` ]
+                        }
+
+                        if ( this.submit_type == "register" && await this.client.nick_exists(it) ) {
                             return [ false, `${it} is occupied` ]
                         }
 
                         return [ true, null ]
-                    } }/>
-                <ValidInput
-                    ref = { (ref) => { this.input_password = ref } }
-                    name = { "password" }
-                    key = { "password" }
-                    secureTextEntry = { true }
-                    autoCompleteType = { "password" }
-                    returnKeyType = { "done" }
-                    onSubmitEditing = { () => this.input_password.blur() }
-                    style = {[  style.generic.text_field_ui, style.login.input ]}
-                    validator = { async (it) => {
+                    }
+                }/>
+        )
+    }
+
+    get field_email() {
+        return (
+            <ValidInput
+                ref = { (ref) => { this.input_email = ref } }
+                name = { "email" }
+                key = { "email" }
+                autoCompleteType = { "email" }
+                returnKeyType = { "next" }
+                onSubmitEditing = { () => this.input_password.focus() }
+                style = {[  style.generic.text_field_ui, style.login.input ]}
+                validator = {
+                    async (it) => {
+                        if (!email_regex.test(it)) {
+                            return [ false, `${it} isn't valid` ]
+                        }
+
+                        if ( this.submit_type == "register" && await this.client.email_exists(it) ) {
+                            return [ false, `${it} is occupied` ]
+                        }
+
+                        return [ true, null ]
+                    }
+                }/>
+        )
+    }
+
+    get field_password() {
+        return (
+            <ValidInput
+                ref = { (ref) => { this.input_password = ref } }
+                name = { "password" }
+                key = { "password" }
+                secureTextEntry = { true }
+                autoCompleteType = { "password" }
+                returnKeyType = { "done" }
+                onSubmitEditing = { () => this.input_password.blur() }
+                style = {[  style.generic.text_field_ui, style.login.input ]}
+                validator = {
+                    async (it) => {
                         if (it.length < 8) {
                             return [ false, `password too short ${it.length} < 8` ]
                         }
 
                         return [ true, null ]
-                    } }/>
+                    }
+                }/>
+        )
+    }
+
+    get fields() {
+        return (
+            <View style = { style.login.input_contain }>
+                { this.submit_type == "register" ? this.field_nick : null }
+                { this.field_email }
+                { this.field_password }
             </View>
         )
+    }
+
+    get profiles() {
+        return <View/>
+    }
+
+    get inputs() {
+        return this.submit_type == "continue" ? this.profiles : this.fields
     }
 
     get submit_button() {
@@ -211,9 +260,9 @@ class LoginView extends HeadedView {
             <TalkingButton
                 ref = { ref => this.submit_button_ref = ref }
                 disabled = { this.submit_disabled }
-                onPress = { () => { this.submit() } }
-                style = {[ style.generic.button_ui, { opacity: this.submit_disabled ? .2 : 1 }]}
-                textStyle = {[ style.login.button_text, style.generic.text_ui]}>
+                onPress = { () => { this.submit_wrapped() } }
+                style = {[ style.generic.button_ui, { opacity: this.submit_disabled ? .2 : 1 } ]}
+                textStyle = {[ style.login.button_text, style.generic.text_ui ]}>
                 { "submit" }
             </TalkingButton>
         )
