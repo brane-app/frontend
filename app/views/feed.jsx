94cbda14ce01367491230eb7256160e96bc93dd2
Noah Harris <mail@gastrodon.io>
2021-01-19 22:48:11 -0500

    Use RecyclerListView for the feed

--- app/views/feed.jsx
+++ app/views/feed.jsx
@@ -1,35 +1,36 @@
-import React, { useRef } from "react"
-import {
-    Animated,
-    View,
-    ScrollView,
-} from "react-native"
+import React from "react"
+import { View } from "react-native"
+import { DataProvider, LayoutProvider, RecyclerListView } from "recyclerlistview"
 
 import imonke from "imonke"
 
 import HeadedView from "../components/headed_view"
 import Content from "../components/content"
-import global_state from "../state"
-import style_generic from "../style/generic"
-import style_feed from "../style/views/feed"
 
 const style = {
-    generic: style_generic,
-    feed: style_feed,
+    feed: require("../style/views/feed").default,
+    generic: require("../style/generic").default,
 }
 
 class FeedView extends HeadedView {
     constructor(opts = {}) {
         super(opts)
-        this.border = 3
-        this.chunk = 30
+        this.chunk = 10
         this.head = ""
-        this.tail = ""
-        this.client = opts.client || global_state.client
+        this.terminated = false
+        this.layout_provider = new LayoutProvider(
+            (index) => { return 0 }, // idk it's just images bro
+            (type, it) => {
+                it.height = 400
+                it.width = this.screen_width
+            }
+        )
+        this.row_renderer = this.row_renderer.bind(this)
+
+        this.data_provider = new DataProvider((it, next) => { it == next })
         this.state = {
-            ...this.state,
-            screen_width: 0, screen_offset: 0,
-            index: 0, buffer: [], drawable_buffer: []
+            ...this.state, buffer: [], screen_width: 1080,
+            data_provider_state: null
         }
     }
 
@@ -37,88 +38,82 @@ class FeedView extends HeadedView {
         return "Feed"
     }
 
-    get screen_width() {
-        return this.state.screen_width
-    }
-
-    get screen_offset() {
-        return this.state.screen_offset
-    }
-
-    get index() {
-        return this.state.index
-    }
-
     get buffer() {
         return this.state.buffer
     }
 
-    get drawable_buffer() {
-        return this.state.drawable_buffer
+    get screen_width() {
+        return this.state.screen_width
     }
 
-    async reset() {
-        this.set_state_safe({ buffer: [], drawable_buffer: [] }, () => this.grow_buffer())
+    get data_provider_state() {
+        return this.state.data_provider_state
     }
 
     async grow_buffer() {
-        if (this.index < this.buffer.length - this.border) {
+        if (this.terminated) {
             return
         }
 
-        let fetched = await this.feed.get({
+        const fetched = await this.feed.get({
             size: this.chunk,
             before: this.head,
         })
-        let buffer = [...this.buffer, ...fetched]
-        let drawable_buffer = [
-            ...this.drawable_buffer,
-            ...fetched.map(it => (
-                <Content
-                    content = {it}
-                    screen_width = {this.screen_width}
-                    key = {`${Math.random()}`}/>
-            ))
-        ]
 
-        this.head = fetched.length == 0 ? "" : await fetched[fetched.length - 1].id
-        this.set_state_safe({
-            buffer: [...this.buffer, ...fetched],
-            drawable_buffer: drawable_buffer,
-        })
-    }
-
-    async handle_scroll(offset) {
-        let snapped = Math.trunc((offset + this.screen_width) % this.screen_width) == 0
-        let movement = Math.round((offset - this.screen_offset) / this.screen_width)
-
-        if (!snapped || Math.abs(movement) == 0) {
+        if (fetched.length === 0) {
+            this.terminated = true
             return
         }
 
+        const grown = [...this.buffer, ...fetched]
+
+        this.head = await fetched[fetched.length - 1].id
         this.set_state_safe({
-            ...this.state,
-            screen_offset: offset,
-            index: this.index + movement,
-        }, () => { this.grow_buffer() })
+            buffer: [...this.buffer, ...fetched],
+            data_provider_state: this.data_provider.cloneWithRows(grown)
+        })
+    }
+
+    async reset() {
+        this.set_state_safe({
+            buffer: [], data_provider_state: null,
+        })
+    }
+
+    row_renderer(_, it) {
+        return (
+            <Content
+                content = { it }
+                screen_width = { 400 } // TODO
+                key = { `${Math.random()}` }/> // TODO get the id somehow
+        )
     }
 
     get scroller() {
+        if (this.data_provider_state === null) {
+            this.grow_buffer()
+            return null
+        }
+
         return (
-            <ScrollView
-                style = {[style.feed.image_scroller]}
-                onLayout = { event => this.set_state_safe({ screen_width: event.nativeEvent.layout.width }) }
-                onScroll = { event => this.handle_scroll(event.nativeEvent.contentOffset.x) }>
-                {this.drawable_buffer}
-            </ScrollView>
+            <RecyclerListView
+                onEndReached = { () => this.grow_buffer() }
+                onEndReachedThreshold = { 300 }
+                onLayout = { event => this.set_state_safe(
+                    {screen_width: event.nativeEvent.layout.width}
+                ) }
+                forceNonDeterministicRendering = { true }
+                dataProvider = { this.data_provider_state }
+                layoutProvider = { this.layout_provider }
+                rowRenderer = { this.row_renderer }
+                />
         )
     }
 
     get content() {
         return (
             <View
-                style = {[style.generic.view]}
-                onLayout = {() => { this.grow_buffer() }}>
+                style = {[style.generic.view]}>
                 {this.scroller}
             </View>
         )
