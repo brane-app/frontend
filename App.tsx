d85cd08685de94df7bc249833cda8512af0585c7
Eva Harris <mail@gastrodon.io>
2021-11-24 18:48:53 -0500

    Draw login by default

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Login } from "./app/view";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App = () => (
+  <View>
+    <StatusBar style={"light"} translucent={false} />
+    <Login />
+  </View>
+);
