5533c37c3637c522f97b4305ef2548810238e5b6
Eva <mail@gastrodon.io>
2022-10-18 13:18:02 -0400

    Navigate past gate if we're authed

--- app/view/auth-gate.tsx
+++ app/view/auth-gate.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Button, View } from "react-native";
+import { store } from "../library/events/store";
 
-export default (props) => (
-  <View>
-    <Button title={"register"} onPress={() => props.navigation.navigate("Register")} />
-    <Button title={"log in"} onPress={() => props.navigation.navigate("Login")} />
-    <Button title={"skip"} onPress={() => props.navigation.navigate("Root")} />
-  </View>
-);
+export default (props: any) =>
+  store.getState().auth.authed
+    ? props.navigation.navigate("Root")
+    : (
+      <View>
+        <Button title={"register"} onPress={() => props.navigation.navigate("Register")} />
+        <Button title={"log in"} onPress={() => props.navigation.navigate("Login")} />
+        <Button title={"skip"} onPress={() => props.navigation.navigate("Root")} />
+      </View>
+    );
