3587be204fc077c58594894fc356a585fa19ac77
Eva <mail@gastrodon.io>
2022-10-18 11:34:34 -0400

    Add route setting

--- app/view/root-navigator.tsx
+++ app/view/root-navigator.tsx
@@ -1,40 +1,31 @@
 import React from "react";
-import { View } from "react-native";
+import { NavigationScreenProp } from "react-navigation";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import Feed from "./feed";
 import Auth from "./auth";
 import UserProfile from "./user-profile";
-import { Auth as AuthKind } from "../library/auth";
 
-const on_auth = (props, client) => {/* TODO */ };
+const ROUTE = "Feeds";
 
-const draw_feed_all = (client) =>
-  (props) => <Feed feed={"all"} client={client} {...props} />;
+const draw_feed_all = (props: any) =>
+  <Feed feed={"all"} {...props} />;
 
-const draw_register = (props) => (
-  <Auth
-    kind={AuthKind.register}
-    on_auth={on_auth}
-    {...props}
-  />
-);
+const draw_register = (props: any) =>
+  <Auth kind={"register"} {...props} />;
 
-const draw_profile = (props) =>
-  <UserProfile self={true} id={"our id"} />;
+const draw_self = () =>
+  <UserProfile self={true} />;
 
-export default (props) => {
+export default (props: { route: string; }) => {
   const Drawer = createDrawerNavigator();
 
   return (
     <NavigationContainer independent={true}>
-      <Drawer.Navigator>
-        <Drawer.Screen
-          name="Feeds"
-          component={draw_feed_all(props.route?.params?.client)}
-        />
-        <Drawer.Screen name="Profile" component={draw_profile} />
+      <Drawer.Navigator initialRouteName={props.route ?? ROUTE}>
+        <Drawer.Screen name="Profile" component={draw_self} />
+        <Drawer.Screen name="Feeds" component={draw_feed_all} />
         <Drawer.Screen name="Register" component={draw_register} />
       </Drawer.Navigator>
     </NavigationContainer>
