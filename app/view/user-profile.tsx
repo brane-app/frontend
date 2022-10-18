ac6e0637cc65d299fef446cf07b71dd1e7aec134
Eva <mail@gastrodon.io>
2022-10-18 13:18:34 -0400

    Draw some data on user page view

--- app/view/user-profile.tsx
+++ app/view/user-profile.tsx
@@ -1,12 +1,44 @@
 import React from "react";
+import { ReactElement } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { View, Text } from "react-native";
+import { get_self, User, user_default } from "../library/brane/user";
+import { store } from "../library/events/store";
 
-interface Props {
-  id: string;
-  self: boolean;
-}
+type user_props = { self: true; } | { self: false, user: User; };
 
-export default (props: Props) =>
-  <View style={{ flex: 1 }}>
-    <Text>`profile of {props.id}`</Text>
+const user_card = (user: User): ReactElement => {
+  return (
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <Text>{user.nick}</Text>
+      <Text>since {user.created}</Text>
+    </View>
+  );
+};
+
+export default (props: user_props) => {
+  const [user, set_user] = useState(user_default);
+
+  useEffect(
+    () => {
+      console.log("updating");
+      if (user.id) return;
+
+      if (props.self) {
+        const { auth: { authed, token } } = store.getState();
+        if (!authed) {
+          throw "self view loaded without auth";
+        }
+
+        get_self(token).then((self_user: User) => set_user(self_user));
+      }
+    },
+    [user],
+  );
+
+  return <View style={{ flex: 1 }}>
+    {user_card(user)}
   </View>;
+};
+
